aaa43458ab352102eca1646dd41c3690cebc8bf1
Winter Falls <178199047+FallenWinters@users.noreply.github.com>
2025-08-20 10:38:38 -0400

    Made new class

--- Pauline - Virelia Petitions.py
+++ Pauline - Virelia Petitions.py
@@ -5,6 +5,16 @@ import tkinter as tk
 window = tk.Tk()
 window.title("Virelia - Petitions")
 window.geometry("320x568")
+window.resizable(height=False, width=False)
+window.config()
+
+class Petition:
+    def __init__(self, subject):
+        self.subject = subject
+    
+    def make_petition(self, desc):
+        self.desc = desc
+
 
 
 window.mainloop()
